117cc67f595bebb46260cf268525e77b221c6200
anjali sharma <anjisharma649@gmail.com>
2024-09-04 12:56:40 +0545

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
     <x-frontend.navbar>
     </x-frontend.navbar>
 <div>
-    <div class=" mx-14 overflow-hidden">
+     {{-- hero section --}}
+     <div class=" mx-16 overflow-hidden">
         <div class="container mx-auto p-4">
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <!-- Main Article -->
@@ -60,7 +61,7 @@
                             </p>
                         </div>
                     </div>
-                    <hr>
+
 
                 </div>
 
@@ -112,6 +113,204 @@
                     <hr>
                 </div>
             </div>
+        </div>
+
+    </div>
+
+
+{{-- Qatar --}}
+
+    <div class="grid grid-cols-12 gap-4 mx-16 mb-4 overflow:hidden">
+        <div class="col-span-2 relative">
+            <h1
+                class=" mt-8 text-2xl font-bold before:content-[''] before:block before:w-16 before:h-[2px] before:bg-black before:mb-2">
+            </h1>
+            <h1 class="text-2xl font-bold mb-4">Qatar</h1>
+        </div>
+
+        <div class="col-span-6 grid grid-cols-6 gap-2"> <!-- Reduced gap-4 to gap-2 -->
+            <div class="col-span-3">
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/09/01/Events_Week_Qatar_Living.png?itok=yoLr4mkY"
+                        alt="" class="w-80 h-48">
+                    <h1 class="text-xl font-bold">Events this week September 1-7</h1>
+                </div>
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/08/29/Smartphone_babysitter_Qatar_Living.png?itok=bmVmKE0c"
+                        alt="" class="w-80 h-48">
+                    <h1 class="text-xl font-bold">Events this week September 1-7</h1>
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/08/17/Geocaching_Qatar_Living.jpeg?itok=JLeQkSYv"
+                        alt="" class="w-32 h-16">
+                    <h1 class="text-xs font-semibold">Want to join a global Treasure Hunt? Let’s explore Geocaching</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-4 mt-2"> <!-- Added mt-2 to decrease vertical gap -->
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+        </div>
+
+    </div>
+
+    {{-- mustread --}}
+    <div class="bg-gray-100 p-4  mx-16 mb-4 h-24">
+        <div class="flex items-center space-x-6">
+            <!-- Left section with the heading and line -->
+            <div class=" items-center space-x-2">
+                <div class="border-t-2 border-black w-14"></div>
+                <h1 class="text-lg font-bold">Must Read</h1>
+            </div>
+
+            <!-- Right section with the list of articles -->
+            <div class="flex space-x-8 p-3 text-center">
+                <p class="text-sm font-semibold mr-3">9 Foods You Won't Believe Will Keep You Cool in the Heat!</p>
+                <p class="text-sm font-semibold">Dive into Summer with Indoor Swimming Pools</p>
+                <p class="text-sm font-semibold">6 Tips to Care for your Skin this Summer</p>
+            </div>
+        </div>
+    </div>
+
+{{-- dinning --}}
+    <div class="grid grid-cols-12 gap-4 mx-16 mb-4 overflow:hidden">
+        <div class="col-span-2 relative">
+            <h1
+                class=" mt-8 text-2xl font-bold before:content-[''] before:block before:w-16 before:h-[2px] before:bg-black before:mb-2">
+            </h1>
+            <h1 class="text-2xl font-bold mb-4">Dining</h1>
+        </div>
+
+        <div class="col-span-7 grid grid-cols-7 gap-2"> <!-- Reduced gap-4 to gap-2 -->
+            <div class="col-span-3">
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/09/01/Events_Week_Qatar_Living.png?itok=yoLr4mkY"
+                        alt="" class="w-80 h-48">
+                    <h1 class="text-xl font-bold">Events this week September 1-7</h1>
+                </div>
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/08/29/Smartphone_babysitter_Qatar_Living.png?itok=bmVmKE0c"
+                        alt="" class="w-80 h-48">
+                    <h1 class="text-xl font-bold">Events this week September 1-7</h1>
+                </div>
+            </div>
+            <div class="col-span-4">
+
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+        </div>
+    </div>
+    </div>
+
+
+    {{-- news --}}
+    <div class="grid grid-cols-12 gap-4 mx-16 mb-4 overflow:hidden">
+        <div class="col-span-2 relative">
+            <h1
+                class=" mt-8 text-2xl font-bold before:content-[''] before:block before:w-16 before:h-[2px] before:bg-black before:mb-2">
+            </h1>
+            <h1 class="text-2xl font-bold mb-4">Qatar</h1>
+        </div>
+
+        <div class="col-span-6 grid grid-cols-6 gap-2"> <!-- Reduced gap-4 to gap-2 -->
+            <div class="col-span-3">
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/09/01/Events_Week_Qatar_Living.png?itok=yoLr4mkY"
+                        alt="" class="w-80 h-48">
+                    <h1 class="text-xl font-bold">Events this week September 1-7</h1>
+                </div>
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/08/29/Smartphone_babysitter_Qatar_Living.png?itok=bmVmKE0c"
+                        alt="" class="w-80 h-48">
+                    <h1 class="text-xl font-bold">Events this week September 1-7</h1>
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="mt-4">
+                    <img src="https://files.qatarliving.com/styles/image_h_medium_440x248/s3/post/2024/08/17/Geocaching_Qatar_Living.jpeg?itok=JLeQkSYv"
+                        alt="" class="w-32 h-16">
+                    <h1 class="text-xs font-semibold">Want to join a global Treasure Hunt? Let’s explore Geocaching</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-4 mt-2"> <!-- Added mt-2 to decrease vertical gap -->
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
+            <div class="mt-4 flex gap-4 mb-4">
+                <img src="https://files.qatarliving.com/styles/image_h_small_300x169/s3/post/2024/07/14/Copy%20of%20SINGLE%20IMAGE%20ARTICLE%20COVER%20(18).png?itok=pdXexM1b"
+                    alt="" class="w-40 h-24">
+                <p class="text-sm font-semibold mr-20">9 Indoor Activities to Keep You Active & Entertained This Summer
+                </p>
+            </div>
+            <hr>
         </div>
 
     </div>
