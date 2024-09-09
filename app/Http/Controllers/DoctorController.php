dc24c0527e3038f001d96b9c6e714f9fd58bde00
anjali sharma <anjisharma649@gmail.com>
2024-09-09 11:43:12 +0545

    property complete

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    public function create(SubCategory $subCategory)
+    {
+        return view('frontend.user.propertyList.create', compact('subCategory'));
+    }
+
+    public function store(StorePropertyListRequest $request, SubCategory $subCategory)
+    {
+        $propertyList = PropertyList::create(
+            $request->validated() + [
+                'sub_category_id' => $subCategory->id,
+                'registered_user_id' => Auth::guard('registered-user')->user()->id,
+            ]
+        );
+        foreach ($request->validated(['files']) as $file) {
+            $propertyList->files()->create([
+                'file_name' => $file->getClientOriginalName(),
+                'file' => $file->store('propertyLists/' . Str::slug($request->input('title'), '_'), 'public'),
+                'size' => $file->getSize(),
+                'extension' => $file->getClientOriginalExtension()
+            ]);
+        }
+        alert("form submitted");
+        return back();
+    }
+
+    public function index()
+    {
+        $registeredUser = Auth::guard('registered-user')->user();
+
+        $categories = Category::with(['subCategories.propertyLists' => function ($query) use ($registeredUser) {
+            $query->where('registered_user_id', $registeredUser->id);
+        }])->get();
+
+        return view('frontend.user.propertyList.index', compact('categories'));
+    }
+
+    public function edit(SubCategory $subCategory, PropertyList $propertyList)
+    {
+        return view('frontend.user.propertyList.edit', compact('subCategory', 'propertyList'));
+    }
+
+    public function update(UpdatePropertyListRequest $request, PropertyList $propertyList)
+    {
+        if ($request->hasFile('files')) {
+            foreach ($request->file('files') as $file) {
+                $propertyList->files()->create([
+                    'file_name' => $file->getClientOriginalName(),
+                    'file' => $file->store('propertyLists/' . Str::slug($request->input('title'), '_'), 'public'),
+                    'size' => $file->getSize(),
+                    'extension' => $file->getClientOriginalExtension()
+                ]);
+            }
+        }
+        $propertyList->update($request->validated());
+        return redirect(route('user.property/propertyLists'));
+    }
+
+
+    public function destroy(PropertyList $propertyList)
+    {
+        foreach ($propertyList->files as $file) {
+
+            $this->deleteFile($file->file);
+        }
+        $propertyList->delete();
+        return back();
+    }
+}
---
+++ app/Models/Doctor.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use App\HasReferenceNumber;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class Doctor extends Model
+{
+    use HasFactory, SoftDeletes, Sluggable;
+    use HasReferenceNumber;
+
+    protected $fillable = [
+        'registered_user_id',
+        'sub_category_id',
+        'status',
+        'slug',
+        'position',
+        'reference_no',
+        'doctor_name',
+        'department',
+        'n_m_c_no',
+        'qualification',
+        'o_p_d_schedule',
+        'details',
+        'youtube_link',
+        'clinic_address',
+        'map_url',
+        'twitter_url',
+        'facebook_url',
+        'whats_app_no',
+        'phone_number',
+        'image'
+    ];
+
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
+
+    public function registeredUser()
+    {
+        return $this->belongsTo(RegisteredUser::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($doctor) {
+            $doctor->position = static::max('position') + 1;
+        });
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => $value ? Storage::disk('public')->url($value) : null,
+            set: fn($value) => $value ? $value->store('doctors', 'public') : null,
+        );
+    }
+
+
+}
